c563fc9b6da2ddf540ef69f7a59ecb438373c21b
mellee115 <mellee115@hotmail.com>
2019-11-14 18:14:46 -0500

    progress bar is working.
    -progress bar is moving with the use of the HTML DOM method addEventListener which is waiting for a "playing" event

--- listeningroom.js
+++ listeningroom.js
@@ -23,6 +23,7 @@ appCtrl.prototype.addSong = function(songObject){
 //play method changes the boolean to true to indicate a song is playing
 appCtrl.prototype.playAudio = function(){
 	this.isPlaying = !this.isPlaying;
+
 } 
 
 //pause method changes the boolean back to the state of false indicating no song is playing
@@ -30,12 +31,31 @@ appCtrl.prototype.pauseAudio = function(){
 	this.isPlaying = !this.isPlaying;
 } 
 
-//calculate the fill the for the progress bar
-appCtrl.prototype.updateBar = function(){
-	let aud = document.getElementById('player');
-	let pb = document.getElementById('bar');
+//calculate the fill the for the progress bar using the addEventListner HTML DOM method
+let timer;
+let percent = 0;
+let aud = document.getElementById('player');
 
-	let val = (aud.currentTime / aud.duration) * 100;
-	console.log(val);
-	pb.css('width', val + 'px');
+aud.addEventListener("playing", function(_event) {
+	let duration = _event.target.duration;
+  	updateBar(duration, aud);
+});
+
+aud.addEventListener("pause", function(_event) {
+  	clearTimeout(timer);
+});
+
+let updateBar = function(duration, audioElement) {
+	let progress = document.getElementById("bar");
+	let incr = 10/duration;
+	percent = incr * audioElement.currentTime * 10;
+	progress.style.width = percent + '%';
+	startTimer(duration, audioElement);
+}
+
+let startTimer = function(duration, audioElement){ 
+  if ( percent < 100 ) {
+    timer = setTimeout(function (){
+    	updateBar(duration, audioElement)}, 100);
+  }
 }
